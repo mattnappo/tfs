a2415be4f59b0453d9423f3d5e97d330e527a5b2
xoreo <mattnappo@gmail.com>
2021-01-12 10:19:09 -0500

    Cleanup

--- rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-vim `find . -name '*.c' -exec grep -le '\<mem\>' {} \;`
-vim `find . -name '*.h' -exec grep -le '\<mem\>' {} \;`
-
-vim `find . -name '*.c' -exec grep -le '\<Mem\>' {} \;`
-vim `find . -name '*.h' -exec grep -le '\<Mem\>' {} \;`
-
-vim `find . -name '*.c' -exec grep -le '\<MEM\>' {} \;`
-vim `find . -name '*.h' -exec grep -le '\<MEM\>' {} \;`
